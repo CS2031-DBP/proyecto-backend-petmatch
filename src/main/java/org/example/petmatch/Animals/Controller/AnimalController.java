4baec8b2d8d67acf35aa6a7913ec82e153f8a794
R10-Mat <rayhan.matos@utec.edu.pe>
2025-10-28 16:43:14 -0500

    Final Animal Entity

--- src/main/java/org/example/petmatch/Animals/Controller/AnimalController.java
+++ src/main/java/org/example/petmatch/Animals/Controller/AnimalController.java
@@ -1,11 +1,28 @@
 package org.example.petmatch.Animals.Controller;
 
+import jakarta.validation.ValidationException;
+import lombok.RequiredArgsConstructor;
+import org.example.petmatch.Animals.DTO.AnimalPresentationDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/animales")
 public class AnimalController {
 
+    private final AnimalController animalService;
+
+    @PatchMapping("/{animalName}/asignar/{shelterName}")
+    public ResponseEntity<AnimalPresentationDTO> assignShelterToAnimal(
+            @PathVariable String animalName,
+            @PathVariable String shelterName) throws ValidationException {
+
+        AnimalPresentationDTO updatedAnimal = animalService.assignShelterToAnimal(animalName, shelterName);
+        return ResponseEntity.ok(updatedAnimal);
+    }
 }
