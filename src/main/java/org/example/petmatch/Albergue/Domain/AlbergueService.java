afc78cbd8306ff88a0fc003231dbe4fb91064c6e
R10-Mat <rayhan.matos@utec.edu.pe>
2025-10-27 20:34:06 -0500

    Cambios albergue

--- src/main/java/org/example/petmatch/Albergue/Domain/AlbergueService.java
+++ src/main/java/org/example/petmatch/Albergue/Domain/AlbergueService.java
@@ -35,36 +35,6 @@ public class AlbergueService {
     }
 
 
-    public Albergue newAlbergue(AlbergueRegisterDTO albergueDTO) throws ValidationException {
-        String pwd = albergueDTO.getPassword();
-        if (!Pattern.matches(".*[A-Z].*", pwd) || !Pattern.matches(".*[0-9].*", pwd)) {
-            throw new ValidationException("Password has no alphanumeric characters");
-        }
-
-        Albergue albergue = modelMapper.map(albergueDTO, Albergue.class);
-        if (albergueDTO.getCapacity() < 20) {
-            albergue.setRating(Rating.BAJA);
-            albergue.setCapacity(albergueDTO.getCapacity());
-            albergue.setAvailableSpaces(albergueDTO.getCapacity());
-        }else if (albergueDTO.getCapacity() < 40) {
-            albergue.setRating(Rating.MEDIA);
-            albergue.setCapacity(albergueDTO.getCapacity());
-            albergue.setAvailableSpaces(albergueDTO.getCapacity());
-        }else {
-            albergue.setRating(Rating.ALTA);
-            albergue.setCapacity(albergueDTO.getCapacity());
-            albergue.setAvailableSpaces(albergueDTO.getCapacity());
-        }
-
-        var found = albergueRepository.findByName(albergueDTO.getName());
-        if (found.isPresent()) {
-            throw new ValidationException("Este albergue ya existe");
-        } else {
-            albergueRepository.save(albergue);
-        }
-        return albergue;
-    }
-
     public void deleteAlberguebyName(String name) throws ValidationException {
         if (!StringUtils.hasText(name)) {
             throw new ValidationException("Name is required");
